1fcd54a093c3b6684e0a09e29a6f5134b2e103d3
leonard <leonard@leonarddeMacBook-Pro.local>
2015-03-18 01:34:45 +0800

    demo

---
+++ Imaginarium/Imaginarium/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Imaginarium
+//
+//  Created by leonard on 15-3-18.
+//  Copyright (c) 2015年 leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Imaginarium/Imaginarium/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Imaginarium
+//
+//  Created by leonard on 15-3-18.
+//  Copyright (c) 2015年 leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
